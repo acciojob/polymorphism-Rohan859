e8c685e8165083c8ccec99ce097b4f6d71ca965f
Rohan859 <rohankundu859@gmail.com>
2024-01-12 22:09:22 +0530

    Logic for Polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,31 @@
 package com.driver;
 
-public class Main {
+public class Main
+{
+    class Product
+    {
+        public int product(int x, int y)
+        {
+            return 0;
+        }
 
+        public int product(int x, int y, int z)
+        {
+            return 0;
+        }
+
+        public double product(double x, double y)
+        {
+            return 0.0;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Product p=new Product();
+        p.product(2,5);
+        p.product(1,2,3);
+        p.product(1.5,2.7);
+
+    }
 }
